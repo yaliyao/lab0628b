1a2ba873b3a11251747033b1edfe642d56cf4c2e
yali <yaliyao88@gmail.com>
2017-06-28 16:15:42 +0800

    show messageBox

--- test0628_try2/Form1.cs
+++ test0628_try2/Form1.cs
@@ -13,5 +13,9 @@ namespace test0628_try2 {
         public Form1() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+            MessageBox.Show("yes");
+        }
     }
 }
